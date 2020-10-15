5c5b2f171c9e3e74c3cf378a14f87370ce6f0abd
Clara Daudre-Vignier <71093939+Clara3245@users.noreply.github.com>
2020-10-15 19:40:08 +0100

    Initial_code

---
+++ PROJECT.py
@@ -0,0 +1,24 @@
+import csv
+
+
+def read_data():
+    data = []
+
+    with open('sales.csv', 'r') as sales_csv:
+        spreadsheet = csv.DictReader(sales_csv)
+        for row in spreadsheet:
+            data.append(row)
+    return data
+
+
+def run():
+    data = read_data()
+    sales = []
+    for row in data:
+        sale = int(row['sales'])
+        sales.append(sale)
+    total = sum(sales)
+    print('Total sales: {}'.format(total))
+
+
+run()
