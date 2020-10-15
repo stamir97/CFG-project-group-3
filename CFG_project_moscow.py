69132a07a2c501c771ea4dd91820150df00992b8
stamir97 <62836957+stamir97@users.noreply.github.com>
2020-10-15 21:28:03 +0300

    Add files via upload

---
+++ CFG_project_moscow.py
@@ -0,0 +1,12 @@
+#Must –
+    #1. Collect all of the sales from each month into a single list
+    #2. Output the total sales across all months
+
+#Should –
+    #1. Output a summary of the results to a spreadsheet
+    #2. Calculate the following:
+    #3. Monthly changes as a percentage
+    #4. The average
+    #5. Months with the highest and lowest sales
+    #6. Use a data source from a different spreadsheet (see Useful Resources)
+
